740fa4b226f4e839eb03be0d2f1e700b41124d83
Corey <60900648+CoreyRExtend@users.noreply.github.com>
2020-12-18 09:54:29 -0800

    [ENG-456] - Eng 456 test #25

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
 const util = require('util')
-const { Octokit } = require("@octokit/rest");
+const { Octokit } = require("@octokit/rest")
+const core = require('@actions/core')
 
 async function main () {
     const context = JSON.parse(process.env.GITHUB_CONTEXT)
-    const token = process.env.GITHUB_TOKEN
     const debug = process.env.DEBUG
+    if (debug) {
+        log(context.event)
+        log(context.event.pull_request.lables)
+    }
+    const token = process.env.GITHUB_TOKEN
     const jiraIssue = process.env.JIRA_ISSUE
     const pullRequest = context.event.pull_request
     const {
-        number: pullRequestNumber,
-        title: pullRequestTitle,
         head: { sha }
     } = pullRequest;
 
@@ -20,32 +23,37 @@ async function main () {
         userAgent: '@extend/mergebot'
     })
 
-    if (debug) {
-        log(context.event.pull_request.lables)
-    }
-
     if (!checkLabels(pullRequest.labels)) {
         console.log("No labels on PR, nothing to do...")
         return false
     }
 
-    const commit = `[${jiraIssue}] - ${pullRequestTitle} #${pullRequestNumber}`
+    const commit = `[${jiraIssue}] - ${pullRequest.title} #${pullRequest.number}`
 
+    const { status, data } = await mergePr(octokit, pullRequest, commit, sha)
+        .catch(error => {
+            log(error)
+            core.setFailed(error.message)
+        })
+    log(data)
+    return status === 200
+}
+
+main()
+
+async function mergePr(octokit, pullRequest, commit_title, sha) {
     const mergeParams = {
         owner: pullRequest.base.repo.owner.login,
         repo: pullRequest.base.repo.name,
         pull_number: pullRequest.number,
-        commit_title: commit,
+        commit_title,
         commit_message: "",
         sha,
         merge_method: 'squash'
     }
     const { status, data } = await octokit.pulls.merge(mergeParams)
-    log(data)
-    return status === 200;
+    return { status, data }
 }
-
-main()
 
 function log(toLog) {
     console.log(util.inspect(toLog, {showHidden: false, depth: null}))
