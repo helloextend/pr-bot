c22ce5edbf9b022c71b376a8605db98fa9eef164
Corey Ryan <corey@extend.com>
2020-12-17 23:07:55 -0800

    dump objects

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const util = require('util')
+
 const context = JSON.parse(process.env.GITHUB_CONTEXT)
 
 console.log('Event: ' + context.event)
-console.log('PR: ' + context.event.pull_request.labels)
+console.log('PR Labels?: ' + context.event.pull_request.labels)
+console.log(util.inspect(context.event.pull_request.labels, {showHidden: false, depth: null}))
+
